eea0fe6fab6605ced221e6931515bc1c450e23c8
Shrenik Jain <shreniksakhala930@gmail.com>
2026-05-14 11:29:05 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,24 @@
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if arr[mid] == target:
+            return mid   # element found, return index
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1   # element not found
+
+
+# Example usage
+numbers = [1, 3, 5, 7, 9, 11]
+result = binary_search(numbers, 7)
+print("\n The Array is [ 1,3,5,7,9,11] "\n)
+print("\n The answer of binary search is :\n")
+
+print("Index:", result)
